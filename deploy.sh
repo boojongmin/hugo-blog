8c86f3aacd0609905893b4a73508b3a1ee3afb98
boojongmin <boojongmin@gmail.com>
2020-11-10 10:55:06 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -12,21 +12,25 @@ git push origin master
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
+# Go To Public folder
+rm -rf public
+
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd public
 
+#echo "blog.boojongmin.com" > CNAME
+
 # Add changes to git.
+git init
+
 git add .
 
-# Commit changes.
-msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-	msg="$*"
-fi
-git commit -m "$msg"
+git remote add origin https://github.com/boojongmin/boojongmin.github.io.git
+
+git commit -m "update"
 
 # Push source and build repos.
-git push origin master
+git push origin master --force
