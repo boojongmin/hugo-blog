28398e628a212be9ea3366d2e0b7094afdccdef4
boojongmin <boojongmin@gmail.com>
2020-11-12 22:29:23 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
 
-
-# If a command fails then the deploy stops
-#set -e
-
-git add .
-
-git commit -m "update"
-
-git push origin master
-
-printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
-
 # Go To Public folder
 rm -rf public
 
