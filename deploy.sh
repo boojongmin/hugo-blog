57c40e0d918ed29fb62c6a62285cd2b9c14c9aa2
boojongmin <boojongmin@gmail.com>
2020-11-20 12:10:13 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,6 @@
 #!/bin/sh
-
-# Go To Public folder
-rm -rf public
-
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
-
-# Go To Public folder
-cd public
-
-#echo "blog.boojongmin.com" > CNAME
-
-# Add changes to git.
-git init
-
+hugo
 git add .
-
-git remote add origin https://github.com/boojongmin/boojongmin.github.io.git
-
 git commit -m "update"
-
-# Push source and build repos.
-git push origin master --force
+git push origin master
 
