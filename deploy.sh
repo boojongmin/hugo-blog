eb61aa8ccd8f0aef2a54b5c552f09c61cd54f2e7
boojongmin <boojongmin@gmail.com>
2020-11-23 10:48:09 +0900

    update 2020년 11월 23일 월 오전 10:48:09

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git commit -m "update $(date)"
 git push origin master
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 hugo
-cp -r public/ deploy/
+cp -r public/* boojongmin.github.io/
 cd deploy
 git add .
 git commit -m "update $(date)"
